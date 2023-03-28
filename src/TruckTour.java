c50cd8d125975c9f47f940f0c1caa13d66f47837
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2023-03-28 15:18:46 -0300

    JesseAndCookies

--- src/TruckTour.java
+++ src/TruckTour.java
@@ -57,7 +57,7 @@ class TruckTour {
     }
 }
 
-class Resolucao {
+class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
