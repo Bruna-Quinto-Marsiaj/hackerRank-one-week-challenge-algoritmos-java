6b3032037a0ef5415b55db1e6cb9084fd665f8b3
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2023-03-28 09:48:18 -0300

    RecursiveDigitSum

---
+++ src/GridChallenge.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class GridChallenge {
+
+    /*
+     * Complete the 'gridChallenge' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING_ARRAY grid as parameter.
+     */
+    public static String sort(String s){
+        char[] ch = s.toCharArray();
+        Arrays.sort(ch);
+        return new String(ch);
+    }
+
+    public static String gridChallenge(List<String> grid) {
+        //grid [ebacd, fghij, olmkn, trpqs, xywuv]
+        // Write your code here
+        ArrayList<String> al = new ArrayList<>();
+        for(int i=0; i<grid.size(); i++){
+            String s = sort(grid.get(i));
+            al.add(s);
+        }
+        //al = [abcde, fghij, klmno, pqrst, uvwxy]
+        for(int i=1; i<al.size(); i++){
+            int size;
+            for(int j = 0; j<size; j++){
+                if(al.get(i-1).charAt(j)>al.get(i).charAt(j)){
+                    return "NO";
+                }
+            }
+        }
+        return "YES";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<String> grid = IntStream.range(0, n).mapToObj(i -> {
+                            try {
+                                return bufferedReader.readLine();
+                            } catch (IOException ex) {
+                                throw new RuntimeException(ex);
+                            }
+                        })
+                        .collect(toList());
+
+                String result = GridChallenge.gridChallenge(grid);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
