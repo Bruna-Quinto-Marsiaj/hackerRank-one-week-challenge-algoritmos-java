fcc78bc49d49579726461a900aa3e12ef23f7c73
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2023-03-29 22:12:31 -0300

    atualizacao codigo PalindromeIndex

--- src/PalindromeIndex.java
+++ src/PalindromeIndex.java
@@ -1,62 +1,74 @@
-
 import java.io.*;
 import java.math.*;
 import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.function.*;
 import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class PalindromeIndex {
 
-    // Complete the palindromeIndex function below.
-    static int palindromeIndex(String s) {
-        if (isPalindrom(s)) { return -1; }
-        int i = 0;
-        while (i < s.length() / 2 && s.charAt(i) == s.charAt(s.length() - 1 - i)) {
-            i++;
-        }
-        String left = removeIndex(s, i);
-        String right = removeIndex(s, s.length() - 1 - i);
-        if (isPalindrom(left)) { return i; }
-        if (isPalindrom(right)) { return s.length() - 1 - i; }
+
+class Result010 {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s) {
+        // Write your code here
+        if (isPalindrome(s)) { return -1; }
+
+        int index = IntStream.range(0, s.length() / 2)
+                .filter(i -> s.charAt(i) != s.charAt(s.length() - 1 - i))
+                .findFirst().orElse(-1);
+
+        if (index == -1) { return -1; }
+
+        String left = s.substring(0, index) + s.substring(index + 1);
+        if (isPalindrome(left)) { return index; }
+
+        String right = s.substring(0, s.length() - 1 - index) + s.substring(s.length() - index);
+        if (isPalindrome(right)) { return s.length() - 1 - index; }
+
         return -1;
     }
 
-    static String removeIndex(String s, int index) {
-        String left = s.substring(0, index);
-        String right = s.substring(index + 1, s.length());
-        return left + right;
+    static boolean isPalindrome(String s) {
+        return IntStream.range(0, s.length() / 2)
+                .allMatch(i -> s.charAt(i) == s.charAt(s.length() - 1 - i));
     }
+}
 
-    static boolean isPalindrom(String s) {
-        for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                return false;
-            }
-        }
-        return true;
-    }
 
-    private static final Scanner scanner = new Scanner(System.in);
 
+public class PalindromeIndex {
     public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int q = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
 
-        for (int qItr = 0; qItr < q; qItr++) {
-            String s = scanner.nextLine();
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
 
-            int result = palindromeIndex(s);
+                int result = Result010.palindromeIndex(s);
 
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
-        }
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
 
+        bufferedReader.close();
         bufferedWriter.close();
-
-        scanner.close();
     }
 }
