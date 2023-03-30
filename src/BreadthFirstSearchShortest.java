53a525c4c8468d5fdb552e16ac38062200623ac2
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2023-03-29 21:01:15 -0300

    correcao nome da classe BreadthFirstsearchShortest

--- src/BreadthFirstSearchShortest.java
+++ src/BreadthFirstSearchShortest.java
@@ -1,12 +1,6 @@
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
@@ -67,7 +61,7 @@ class Result008 {
 }
 
 
-public class BreadFirstsearchShortest {
+public class BreadthFirstSearchShortest {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
