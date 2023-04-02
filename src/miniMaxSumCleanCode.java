1473dd44ebe937ebee2407b781fb35b63f1b5f70
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2023-04-01 22:27:57 -0300

    miniMaxSumCleanCode

---
+++ src/miniMaxSumCleanCode.java
@@ -0,0 +1,44 @@
+
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+        import static java.util.stream.Collectors.toList;
+
+class Result99 {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        long sum = arr.stream().mapToLong(Integer::longValue).sum();
+        long min = arr.stream().mapToLong(Integer::longValue).min().orElse(0);
+        long max = arr.stream().mapToLong(Integer::longValue).max().orElse(0);
+        System.out.printf("%d %d", sum - max, sum - min);
+    }
+
+}
+
+public class miniMaxSumCleanCode  {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result99.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
