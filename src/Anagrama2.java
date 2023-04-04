90fbd869b9e53f0ee18ed2227ef94cdbb41be4c9
Bruna Quinto Marsiaj <110314990+Bruna-Quinto-Marsiaj@users.noreply.github.com>
2023-04-04 09:26:34 -0300

    Anagrama2 correcao

--- src/Anagrama2.java
+++ src/Anagrama2.java
@@ -24,22 +24,20 @@ class Resultggb {
     public static int anagram(String s) {
         // Write your code here
         int n = s.length();
-        if (n % 2 != 0){
+        if (n % 2 != 0) {
             return -1;
         }
-        String s1 = s.substring(0, n/2);
-        String s2 = s.substring(n/2);
-        Map<Character, Integer> freq = new HashMap<>();
-
-        s1.chars(). forEach(c -> freq.put((char) c, freq.getOrDefault((char) c, 0) + 1));
+        int[] freq = new int[26];
+        for (int i = 0; i < n / 2; i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
         int changes = 0;
-
-        for(int i = 0; i < s2.length(); i++){
-            char c = s2.charAt(i);
-            if(!freq.containsKey(c) || freq.get(c) == 0){
+        for (int i = n / 2; i < n; i++) {
+            int index = s.charAt(i) - 'a';
+            if (freq[index] == 0) {
                 changes++;
             } else {
-                freq.put(c, freq.get(c) - 1);
+                freq[index]--;
             }
         }
         return changes;
@@ -48,7 +46,7 @@ class Resultggb {
 
 }
 
-public class Anagram2 {
+public class Anagrama2 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
